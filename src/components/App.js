74486e5d1861eca37e4f3862caf084d4236ced57
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2024-03-22 07:12:51 +0000

    Usestate

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
 
 import React from "react";
 import './../styles/App.css';
-//import counter from './counter';
+
 import {useState} from "react";
 
 const App = () => {
   //using useState
   const [count,setCount]=useState(0);
   const increment=() =>{
-    let curr=count+1;
-    setCount(curr);
+ //   let curr=count+1;
+    setCount(count+1);
   }
   return (
     <div>
