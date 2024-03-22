318ca42b1ae0a4c7586ba8cede170f2cc3a5f6ff
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2024-03-22 10:03:39 +0000

    parthensee

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,40 @@
 
-import React from "react";
+// import React from "react";
+// import './../styles/App.css';
+
+// import {useState} from "react";
+
+// const App = () => {
+//   //using useState
+//   const [count,setCount]=useState(0);
+//   const increment=() =>{
+//  //   let curr=count+1;
+//     setCount(count+1);
+//   }
+//   return (
+//     <div>
+//       {/* <Counter/> */}
+//       <p>Button clicked {count} times</p>
+//       <button onClick={increment}>Click me</button>
+//     </div>
+//   )
+// }
+
+
+import React, {useState} from "react";
 import './../styles/App.css';
-
-import {useState} from "react";
-
 const App = () => {
-  //using useState
-  const [count,setCount]=useState(0);
-  const increment=() =>{
- //   let curr=count+1;
-    setCount(count+1);
+  const [count , setCount] = useState(0);
+  const onClickEvent = () => {
+    setCount(count + 1);
   }
   return (
     <div>
-      {/* <Counter/> */}
-      <p>Button clicked {count} times</p>
-      <button onClick={increment}>Click me</button>
+        {/* Do not remove the main div */}
+        <p>{`Button clicked ${count} times`}</p>
+        <button onClick={onClickEvent}>Click me</button>
     </div>
+    
   )
 }
-
+export default App
