7bb498e0a85bc82f2dea013aae17f50f7b348739
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2024-03-22 07:09:17 +0000

    srinija

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 
 import React from "react";
 import './../styles/App.css';
-import counter from './counter';
+//import counter from './counter';
+import {useState} from "react";
 
 const App = () => {
+  //using useState
+  const [count,setCount]=useState(0);
+  const increment=() =>{
+    let curr=count+1;
+    setCount(curr);
+  }
   return (
     <div>
-      <Counter/>
+      {/* <Counter/> */}
+      <p>Button clicked {count} times</p>
+      <button onClick={increment}>Click me</button>
     </div>
   )
 }
